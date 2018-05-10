a9c762226b151d235162dc6058d64ea2a6da198c
Richard O'Dwyer <richard@richard.do>
2018-05-10 14:05:03 +0100

    packaging

--- setup.py
+++ setup.py
@@ -10,22 +10,22 @@ try:
        as build_py
 except ImportError:
    from distutils.command.build_py import build_py
-   
+
 try:
    from Cython.Distutils import build_ext
 except ImportError:
    use_cython = False
 else:
    use_cython = True
-   
-   
+
+
 #### data files
 data_files = []
 # Include gsl libs for the win32 distribution
 if get_platform() == "win32":
    dlls = ["gsl.lib", "cblas.lib"]
    data_files += [("Lib\site-packages\mlpy", dlls)]
-   
+
 #### libs
 if get_platform() == "win32":
    gsl_lib = ['gsl', 'cblas']
@@ -33,13 +33,13 @@ if get_platform() == "win32":
 else:
    gsl_lib = ['gsl', 'gslcblas']
    math_lib = ['m']
-   
+
 #### Extra compile args
 if get_platform() == "win32":
    extra_compile_args = []
 else:
    extra_compile_args = ['-Wno-strict-prototypes']
-   
+
 #### Python include
 py_inc = [get_python_inc()]
 
@@ -117,12 +117,12 @@ if use_cython:
                              ["mlpy/findpeaks/c_findpeaks.c",
                               "mlpy/findpeaks/findpeaks.pyx"],
                              include_dirs=py_inc + np_inc),
-                   Extension("mlpy.dtw", 
+                   Extension("mlpy.dtw",
                              ["mlpy/dtw/cdtw.c",
                              "mlpy/dtw/dtw.pyx"],
                              libraries=math_lib,
                              include_dirs=py_inc + np_inc),
-                   Extension("mlpy.lcs", 
+                   Extension("mlpy.lcs",
                              ["mlpy/lcs/clcs.c",
                               "mlpy/lcs/lcs.pyx"],
                              libraries=math_lib,
@@ -200,7 +200,7 @@ else:
                              libraries=math_lib,
                              include_dirs=py_inc + np_inc),
                    ]
-   
+
 # pure c modules
 ext_modules += [Extension("mlpy.wavelet._dwt",
                           ["mlpy/wavelet/dwt.c"],
@@ -250,15 +250,15 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Operating System :: POSIX'
                ]
 
-setup(name = 'mlpy',
+setup(name = 'machine-learning-py',
       version='3.5.0',
       requires=['numpy (>=1.3.0)', 'scipy (>=0.7.0)', 'gsl (>=1.11)'],
       description='mlpy - Machine Learning Py - ' \
          'High-Performance Python Package for Predictive Modeling',
       author='mlpy Developers',
       author_email='davide.albanese@gmail.com',
-      maintainer='Davide Albanese',
-      maintainer_email='davide.albanese@gmail.com',
+      maintainer='Richard O\'Dwyer',
+      maintainer_email='richard@richard.do',
       packages=packages,
       url='mlpy.sourceforge.net',
       download_url='https://sourceforge.net/projects/mlpy/',
